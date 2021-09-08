21bb3c3552eb219c566c730b56ce6667d6d97a53
rickyjames1750 <rickyjames1750@gmail.com>
2021-09-08 14:29:09 -0700

    Create CameraViewController.swift

---
+++ Rubae/Controllers/Core Tabs/CameraViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CameraViewController.swift
+//  Rubae
+//
+//  Created by Ricky Sparks on 9/8/21.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
