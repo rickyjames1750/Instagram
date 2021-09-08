59dab87c6bd326522e5d17dc323f25d7d59c3d16
rickyjames1750 <rickyjames1750@gmail.com>
2021-09-08 14:29:28 -0700

    Create NotificationsViewController.swift

---
+++ Rubae/Controllers/Core Tabs/NotificationsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  NotificationsViewController.swift
+//  Rubae
+//
+//  Created by Ricky Sparks on 9/8/21.
+//
+
+import UIKit
+
+class NotificationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
