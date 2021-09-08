5ec3d304f1b5c2a96114401e65f9291e45f56c6d
rickyjames1750 <rickyjames1750@gmail.com>
2021-09-08 14:29:16 -0700

    Create CitizenViewController.swift

---
+++ Rubae/Controllers/Core Tabs/CitizenViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CitizenViewController.swift
+//  Rubae
+//
+//  Created by Ricky Sparks on 9/8/21.
+//
+
+import UIKit
+
+class CitizenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
