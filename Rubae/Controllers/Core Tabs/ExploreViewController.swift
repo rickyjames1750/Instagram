bda371897a5e47a1ee75bfeb276d0a1bede143fc
rickyjames1750 <rickyjames1750@gmail.com>
2021-09-08 14:29:20 -0700

    Create ExploreViewController.swift

---
+++ Rubae/Controllers/Core Tabs/ExploreViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ExploreViewController.swift
+//  Rubae
+//
+//  Created by Ricky Sparks on 9/8/21.
+//
+
+import UIKit
+
+class ExploreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
