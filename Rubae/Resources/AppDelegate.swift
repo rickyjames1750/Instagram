01407cf6ca8ea0beab90cb522b59eb8a9df64488
rickyjames1750 <rickyjames1750@gmail.com>
2021-09-10 15:28:04 -0700

    Update AppDelegate.swift

--- Rubae/Resources/AppDelegate.swift
+++ Rubae/Resources/AppDelegate.swift
@@ -5,17 +5,18 @@
 //  Created by Ricky Sparks on 9/7/21.
 //
 
+
 import Firebase
+import FirebaseAnalytics
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
         FirebaseApp.configure()
+        
         return true
     }
 
