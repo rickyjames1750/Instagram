e6aabc0daabd2538e5af94070c0ffbc75af8b348
rickyjames1750 <rickyjames1750@gmail.com>
2021-09-10 23:06:22 -0700

    Major file change and deletions

--- Rubae/Resources/AppDelegate.swift
+++ Rubae/Resources/AppDelegate.swift
@@ -6,24 +6,21 @@
 //
 
 
-import Firebase
 import UIKit
-//import FirebaseAnalytics
-//import FirebaseDatabase
-//import FirebaseCore
+//import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
-        FirebaseApp.configure()
-        
+        // Override point for customization after application launch.
+       // FirebaseApp.configure()
         return true
     }
 
     // MARK: UISceneSession Lifecycle
-
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
@@ -38,4 +35,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 }
-
